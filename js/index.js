8c099989cf1de6a9a45f5001a653d172530614c8
Akira Jaqueline Joya Hernández <23030862@itecleaya.edu.mx>
2026-04-23 11:38:06 -0600

    Persistencia de preferencia

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ import View from './view.js';
 document.addEventListener('DOMContentLoaded', () => {
   const model = new Model();
   const view = new View();
-  const themeManager = new ThemeManager();
+  //Inicializar el gestor de temas para aplicar el tema guardado en localStorage
+  const themeManager = new ThemeManager(); 
   model.setView(view);
   view.setModel(model);
 
