596c76b2ed0536dd4517d797ac272469f6815516
Akira Jaqueline Joya Hernández <23030862@itecleaya.edu.mx>
2026-04-23 11:30:21 -0600

    Existencia del control de cambio de tema

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import View from './view.js';
 document.addEventListener('DOMContentLoaded', () => {
   const model = new Model();
   const view = new View();
+  const themeManager = new ThemeManager();
   model.setView(view);
   view.setModel(model);
 
